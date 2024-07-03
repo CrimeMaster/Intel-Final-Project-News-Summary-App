30ca563532b87c547b77aba554163d4e09dee748
CrimeMaster <gtirkey300@gmail.com>
2024-07-03 10:12:06 +0530

    Basic skeletal structure working

--- server/config/database.py
+++ server/config/database.py
@@ -1,6 +1,12 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-client = MongoClient("mongodb+srv://admin:test1234!@cluster0.szfvkcc.mongodb.net/?appName=Cluster0")
+load_dotenv()
+
+mongo_uri = "mongodb+srv://admin:test1234!@cluster0.szfvkcc.mongodb.net/?appName=Cluster0"
+
+client = MongoClient(mongo_uri)
 
 db = client.NewsList
 
